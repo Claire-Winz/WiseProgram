ac72ec06e94c44f76d76beb82c752b88cad0dda2
Winz <airspirk@gmail.com>
2023-07-31 11:35:16 +0900

    WiseProgram

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import Controller.Controller;
 
-import javax.sound.sampled.Control;
-
 public class Main {
     public static void main(String[] args) {
         Controller start = new Controller();
